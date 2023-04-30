448f53ecdb78eae70434b81c1209854581660e72
Dinesh <86416683+crazyboz@users.noreply.github.com>
2023-04-30 13:29:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express =require("express")
 const app=express()
-const cors=require("cors")
+// const cors=require("cors")
 const mongoose=require("mongoose")
 const {getData,create,update,deletes}=require("./components/Curd")
 
@@ -14,20 +14,20 @@ mongoose.connect("mongodb+srv://dineshn20:PfpP1VIFgSTPaZxw@cluster0.iv9wg6d.mong
 
 //database 
 
-var allowlist = ['https://crazyboz.github.io/Todo_List-MERN/']
-var corsOptionsDelegate = function (req, callback) {
-  var corsOptions;
-  if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-  } else {
-    corsOptions = { origin: false } // disable CORS for this request
-  }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
+// var allowlist = ['https://crazyboz.github.io/Todo_List-MERN/']
+// var corsOptionsDelegate = function (req, callback) {
+//   var corsOptions;
+//   if (allowlist.indexOf(req.header('Origin')) !== -1) {
+//     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+//   } else {
+//     corsOptions = { origin: false } // disable CORS for this request
+//   }
+//   callback(null, corsOptions) // callback expects two parameters: error and options
+// }
 
 
 app.use(express.json())
-app.use(cors(corsOptionDelegate))
+// app.use(cors(corsOptionDelegate))
 
 app.get("/data/:id",getData)
 
