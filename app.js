b08d40ccfe00d082ff354cc01a10c7dec9aeeaef
Dinesh <86416683+crazyboz@users.noreply.github.com>
2023-04-30 13:12:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,6 +16,18 @@ app.use(express.json())
 app.use(cors())
 //database 
 
+var allowlist = ['https://crazyboz.github.io/Todo_List-MERN/']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+
 app.get("/data/:id",getData)
 
 app.post("/data",create)
